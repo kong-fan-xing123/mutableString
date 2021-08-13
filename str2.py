e79ac3d987783ef8fa6e20b192ef35500f971973
kong-fan-xing123 <kfstary@outlook.com>
2021-08-13 12:11:07 +0800

    Create str2.py

---
+++ str2.py
@@ -0,0 +1,24 @@
+#usr/bin/python3.10
+# use for python3.10
+# 适用于Python3.10
+class String():
+	def __init__(self, object: str | bytearray=''):
+		self.__listofthestring = list(object)
+	def __getitem__(self,index):
+		return self.__listofthestring[index]
+	def __setitem__(self,index,value):
+		self.__listofthestring[index] = value
+	def __str__(self) -> str:
+		return ''.join(self.__listofthestring)
+	def __repr__(self) -> str:
+		return "'" + ''.join(self.__listofthestring) + "'"
+	def __eq__(self, o: object) -> bool:
+		return ''.join(self.__listofthestring) == o
+	def __ne__(self, o: object) -> bool:
+		return not self == o
+	def __add__(self,o):
+		return self.__class__(''.join(self.__listofthestring) + str(o))
+	def __radd__(self,o):
+		return self.__class__(''.join(self.__listofthestring) + str(o))
+
+
